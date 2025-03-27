643e441058bcfffc0abf9fb0e973e5647a568285
Noorhesham <noordragon2004@gmail.com>
2025-03-27 10:24:44 +0200

    last

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     serverActions: {
-      bodySizeLimit: "300mb",
+      bodySizeLimit: "500mb",
     },
   },
   typescript: {
